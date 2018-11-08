1dfe58ee456550476b72c60de29fd68ab81ee447
Vasylyna <vasylyna.kit@gmail.com>
2018-11-08 14:33:20 +0200

    imported FormsModule, ReactiveFormsModule, HttpModule into app.module.ts; imported FormBuilder, FormGroup, Validators into app.component.ts; created properties which will be displayed on the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactive';
+  rForm:FormGroup;
+  post:any;
+  description:string = '';
+  name:string = '';
+  
+ 
 }
